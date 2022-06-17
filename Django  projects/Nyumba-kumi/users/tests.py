633ec3522d74dad9400624042ab06360dc494a80
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-17 23:15:31 +0300

    ft:write tests

--- Django  projects/Nyumba-kumi/users/tests.py
+++ Django  projects/Nyumba-kumi/users/tests.py
@@ -1,31 +1,31 @@
 from django.test import TestCase
-from .models import Jirani, Business
+from .models import Neighbourhood, Business
 
 # Create your tests here.
-class JiraniTestClass(TestCase):
+class NeighbourhoodTestClass(TestCase):
     def setUp(self):
-          self.new_Jirani = Jirani( name='west', location='langata', health_dept= '911', police_dept= '911', logo= 'random.jpg')
+          self.new_neighbourhood = Neighbourhood( name='west', location='langata', health_dept= '911', police_dept= '911', logo= 'random.jpg')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_neighbourhood, Neighbourhood))
 
     def tearDown(self):
-      JiraniTestClass.objects.filter(id=1).delete()
+      Neighbourhood.objects.filter(id=1).delete()
 
-    def test_create_Jirani(self):
-        self.new_Jirani.create_Jirani()
-        self.assertTrue(len(Jirani.objects.all())>0)
+    def test_create_neighbourhd(self):
+        self.new_neighbourhood.create_neighbourhd()
+        self.assertTrue(len(Neighbourhood.objects.all())>0)
 
-    def test_delete_Jirani(self):
-        self.new_Jirani.delete_Jirani(1)
-        self.assertTrue(len(Jirani.objects.all()) == 0)
+    def test_delete_neighbourhd(self):
+        self.new_neighbourhood.delete_neighbourhd(1)
+        self.assertTrue(len(Neighbourhood.objects.all()) == 0)
 
-    def test_find_Jirani(self):
-        self.new_Jirani.create_Jirani()
-        self.assertTrue(len(Jirani.objects.filter(id=1)) == 1)
+    def test_find_neighbourhd(self):
+        self.new_neighbourhood.create_neighbourhd()
+        self.assertTrue(len(Neighbourhood.objects.filter(id=1)) == 1)
 
-    def test_update_Jirani(self):
-        self.new_Jirani.create_Jirani()
+    def test_update_neighbourhd(self):
+        self.new_neighbourhood.create_neighbourhd()
         pass
 
 class BusinessTestClass(TestCase):
