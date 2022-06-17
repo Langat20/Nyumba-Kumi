7d7bc157902644cdd6daea5bf530f60149092560
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-17 23:20:01 +0300

    ft:create model class business

--- Django  projects/Nyumba-kumi/Jirani/models.py
+++ Django  projects/Nyumba-kumi/Jirani/models.py
@@ -44,3 +44,35 @@ class Profile(models.Model):
     def __str__(self):
         return self.name
 
+class Business(models.Model):
+    name = models.CharField(max_length=60)
+    logo = CloudinaryField('image', blank=True)
+    description = models.TextField(max_length=200, blank=True)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self):
+      return self.name
+
+    def create_busn(self):
+        self.save()
+
+    @classmethod
+    def delete_busn(cls, pk):
+        cls.objects.filter(pk=pk).delete()
+
+    @classmethod
+    def find_busn(cls, id):
+        search_results = cls.objects.filter(id=id)
+        return search_results
+
+    def update_busn(self, name):
+      self.name = name
+      self.save()
+
+    @classmethod
+    def search_business(cls,search_term):
+        businesses = cls.objects.filter(name__icontains=search_term)
+        return businesses
+
