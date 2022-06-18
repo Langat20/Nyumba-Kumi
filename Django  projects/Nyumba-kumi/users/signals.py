3798e13c1fe0dfbbf3ed5794dda73318d8feb2ad
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-18 08:23:31 +0300

    function to save a user profile for each new user

--- Django  projects/Nyumba-kumi/users/signals.py
+++ Django  projects/Nyumba-kumi/users/signals.py
@@ -14,3 +14,9 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    '''
+    function to save a user profile for each new user
+    '''
+    instance.profile.save()
