90f73be0ea953c2ee4133cf2e24aed7b14041440
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-18 08:10:38 +0300

    create class EmailReceipient

--- Django  projects/Nyumba-kumi/users/models.py
+++ Django  projects/Nyumba-kumi/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class EmailRecipients(models.Model):
+    '''
+    EmailRecepients model acts as blueprint for all email recepients on registation
+    '''
+
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    
